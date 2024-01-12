c57273e514aa1a20e6f8760d2e3715339c0c8b45
ahmadabuesmaeel97 <ahmad.abu.esmaeel97@gmail.com>
2024-01-12 17:03:18 +0200

    OMGR-52 - Taqa (AK) upgrade 14 to 16[Fix on change price per unit on slae order line]

--- addons/taqa_custom_module/model/account_move.py
+++ addons/taqa_custom_module/model/account_move.py
@@ -25,8 +25,9 @@ class AccountMoveLine(models.Model):
         for rec in self:
             if rec.product_uom_id:
                 rec.price_unit = rec.price_per_unit * rec.product_uom_id.x_kgexch
-
-
+            else:
+                rec.price_unit = rec.price_per_unit
+    
 class SaleOrderPo(models.Model):
     _inherit = 'sale.order'
 
